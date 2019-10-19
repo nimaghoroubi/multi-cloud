20fff45a04c472a016abedf72a3a3cc96320dc4b
nimaghoroubi <nima.ghoroubi@gmail.com>
2019-10-19 19:04:36 +0200

    credentials on worker side

--- worker/addfunction.py
+++ worker/addfunction.py
@@ -1,7 +1,7 @@
 from celery import Celery
 
 app = Celery('tasks', backend='amqp',
-broker='amqp://add@34.230.81.244/add')
+broker='amqp://killer:killer@34.230.81.244/killer')
 
 def add(x, y):
     return x + y
