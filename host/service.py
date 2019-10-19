587c50e9acf96b85b353fc0a127323c46fa9695d
nimaghoroubi <nima.ghoroubi@gmail.com>
2019-10-19 18:17:32 +0200

    god help us all

---
+++ host/service.py
@@ -0,0 +1,6 @@
+from remote import add
+from add-function import add
+
+add.delay(10,20)
+add.delay(20,30)
+add.delay(1,2)
---
+++ worker/add-function.py
@@ -0,0 +1,7 @@
+from celery import Celery
+
+app = Celery('tasks', backend='amqp',
+broker='amqp://add@34.230.81.244/add')
+
+def add(x, y):
+return x + y
