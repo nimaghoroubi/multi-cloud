86b1f3dbe00f1eac2887142855d5c3ae3b5c124d
nimaghoroubi <nima.ghoroubi@gmail.com>
2019-10-20 19:39:25 +0200

    minor aesthetics

--- host/userdatawriter.py
+++ host/userdatawriter.py
@@ -33,23 +33,23 @@ def create_file(body):
 
 
 
-            runcmd:
-            -#!/bin/bash
-            - echo "starting ******************************************************************"
-            - echo "installing updates"
-            - sudo apt-get update &&
-            - sudo apt-get -y upgrade &&
-            - echo "installing python"
-            - sudo apt-get -y install python &&
-            - echo "installing and upgrading pip"
-            - sudo apt -y install python-pip &&
-            - sudo python -m pip install --upgrade pip &&
-            - echo "installing celery"
-            - sudo python -m pip install celery &&
-            - echo "services ready, cloning repo"
-            - sudo git clone https://github.com/nimaghoroubi/multi-cloud /multi
-            - echo "clone complete! running services!"
-            - (cd /multi && screen -dmS celery celery worker -l info -A addfunction)
+    runcmd:
+    -#!/bin/bash
+    - echo "starting ******************************************************************"
+    - echo "installing updates"
+    - sudo apt-get update &&
+    - sudo apt-get -y upgrade &&
+    - echo "installing python"
+    - sudo apt-get -y install python &&
+    - echo "installing and upgrading pip"
+    - sudo apt -y install python-pip &&
+    - sudo python -m pip install --upgrade pip &&
+    - echo "installing celery"
+    - sudo python -m pip install celery &&
+    - echo "services ready, cloning repo"
+    - sudo git clone https://github.com/nimaghoroubi/multi-cloud /multi
+    - echo "clone complete! running services!"
+    - (cd /multi && screen -dmS celery celery worker -l info -A addfunction)
             """
 
     with open("./user-data.txt", 'w+') as ud:
