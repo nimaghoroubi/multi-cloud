dfd3ddbe1c39b66d1077589479eb638f13ba35f3
nimaghoroubi <nima.ghoroubi@gmail.com>
2019-10-20 20:47:52 +0200

    didnt work, trying new script creator that works

--- host/userdatawriter.py
+++ host/userdatawriter.py
@@ -1,40 +1,21 @@
 def create_file(body):
     pre = """#cloud-config
-
-# run commands
-# default: none
-# runcmd contains a list of either lists or a string
-# each item will be executed in order at rc.local like level with
-# output to the console
-# - runcmd only runs during the first boot
-# - if the item is a list, the items will be properly executed as if
-#   passed to execve(3) (with the first arg as the command).
-# - if the item is a string, it will be simply written to the file and
-#   will be interpreted by 'sh'
-#
-# Note, that the list has to be proper yaml, so you have to quote
-# any characters yaml would eat (':' can be problematic)
-
 write_files:
   - path: /multi/addfunction.py
     content: |
         from celery import Celery
-
         broker_adress = 'amqp://killer:killer@"""
 
     content = """/killer'
-
         app = Celery('tasks', backend='amqp',
         broker = broker_adress)
-
         @app.task
         def add(x, y):
             return x + y
 
 
 
-    runcmd:
-    -#!/bin/bash
+runcmd:
     - echo "starting ******************************************************************"
     - echo "installing updates"
     - sudo apt-get update &&
