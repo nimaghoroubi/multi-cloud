cfa91cf01268e1d6e12fcc59bb1f41ee5bbf91dc
nimaghoroubi <nima.ghoroubi@gmail.com>
2019-10-22 20:32:11 +0200

    for some reason aws configure fails

--- host/host-env.sh
+++ host/host-env.sh
@@ -31,6 +31,7 @@ sudo service rabbitmq-server restart &&
 screen -dmS host sudo python /multi/host/flask-app.py &&
 # ######################### installing aws and creating instances with config file
 sudo snap install  aws-cli --classic &&
+sleep 5
 echo "aws installed" &&
 #eu-north-1
 #aws configure set default.region eu-north-1 &&
