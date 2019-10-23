4eb96286e5c30972608430c67c8915c4607090be
Andreas Ivarsson <Ivarsson.Andreas@hotmail.se>
2019-10-23 14:53:10 +0200

    Octave wroks locally

---
+++ tasks_playground.py
@@ -0,0 +1,14 @@
+from oct2py import Oct2Py
+import json
+
+octave = Oct2Py()
+octave.addpath('//Users/andreasivarsson/multi-cloud/BENCHOP/RBF-FD')
+# octave.addpath('/multi/BENCHOP/RBF-FD')
+# octave.addpath('/multi/BENCHOP/RBF-FD')
+
+
+def problem1(S, K, T, r, sig):
+    print('problem1')
+    res = json.dumps(octave.BSeuCallUI_RBFFD(S, K, T, r, sig).tolist())
+    print('res:', res)
+    return res
