d54bdaffa9c31a773020fcdda9434c49f497d8a9
nimaghoroubi <nima.ghoroubi@gmail.com>
2019-10-23 15:21:16 +0200

    new base route

--- host/flask-app.py
+++ host/flask-app.py
@@ -25,10 +25,10 @@ celery = make_celery(app)
 
 @app.route('/')
 def test():
-    request = add_function.delay(10, 20)
-    request.ready()
-    return_value = request.get()
-    return return_value
+    #request = add_function.delay(10, 20)
+    #request.ready()
+    #return_value = request.get()
+    return "You are at the right place\n"
 
 
 @app.route('/problems/<problem_id>',)
